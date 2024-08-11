509d95ac6e62013a53a1ad6e692ec558f625bc72
matheus <matheuz1398@gmail.com>
2024-08-11 19:00:53 -0300

    Update serve.js

--- backend/serve.js
+++ backend/serve.js
@@ -1,1 +1,35 @@
-var n1 = 4
+const input = document.querySelector('#arquivo');
+const preview = document.querySelector('#preview');
+const btndownload = document.querySelector('#download');
+
+input.addEventListener('change', function(){
+    const arquivo = this.files[0];  
+    const leitor = new FileReader();
+
+    leitor.addEventListener('load', function(){
+        console.log(leitor.result);
+        preview.value = leitor.result;
+    });
+
+    if (arquivo) {
+        leitor.readAsText(arquivo);
+    }
+});
+
+const download = function(){
+    const a = document.createElement('a');
+    a.style = 'display: none';  
+    document.body.appendChild(a);  
+    return function(conteudo, nomeArquivo) {
+        const blob = new Blob([conteudo], {type: 'application/octet-stream'}); 
+        const url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = nomeArquivo;
+        a.click();
+        window.URL.revokeObjectURL(url);
+    }
+}
+
+btndownload.addEventListener('click', function(){
+    download()(preview.value, 'erro 404.png'); 
+});
